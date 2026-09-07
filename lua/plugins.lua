3892af8d9e4380a1ff9e473dc93bd03c01830554
xrh999 <xrh999@gmail.com>
2026-09-07 12:44:10 +0800

    add nvim.autopair

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,6 +11,7 @@ vim.pack.add ({
     { src = "https://github.com/mikavilpas/yazi.nvim"},
     { src = "https://github.com/folke/which-key.nvim"},
     { src = "https://github.com/lewis6991/gitsigns.nvim"},
+    { src = "https://github.com/windwp/nvim-autopairs"},
 })
 
 -- set up the theme lol
@@ -97,3 +98,25 @@ require("which-key").setup({
 
 -- Git set up
 require('gitsigns').setup {}
+
+-- autopair set up
+
+require('nvim-autopairs').setup {
+    disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
+    disable_in_macro = true, -- disable when recording or executing a macro
+    disable_in_visualblock = false, -- disable when insert after visual block mode
+    disable_in_replace_mode = true,
+    ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+    enable_moveright = true,
+    enable_afterquote = true, -- add bracket pairs after quote
+    enable_check_bracket_line = true, --- check bracket in same line
+    enable_bracket_in_quote = true, --
+    enable_abbr = false, -- trigger abbreviation
+    break_undo = true, -- switch for basic rule break undo sequence
+    check_ts = false,
+    map_cr = true,
+    map_bs = true, -- map the <BS> key
+    map_c_h = false, -- Map the <C-h> key to delete a pair
+    map_c_w = false, -- map <c-w> to delete a pair if possible
+}
+
